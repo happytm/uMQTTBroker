657cb397fa788cfac703a06f8cabdeb9a1dc61a6
happytm <36576353+happytm@users.noreply.github.com>
2019-11-17 19:13:09 -0500

    Add files via upload

---
+++ examples/ProbeReceiver.ino
@@ -0,0 +1,274 @@
+/*
+ * uMQTTBroker demo for Arduino (C++-style)
+ * 
+ * The program defines a custom broker class with callbacks, 
+ * starts it, subscribes locally to anything, and publishs a topic every second.
+ * Try to connect from a remote client and publish something - the console will show this as well.
+ */
+
+#define  MQTTBROKER           true
+
+
+#include <ESP8266WiFi.h>
+#include "uMQTTBroker.h"
+
+/*
+ * Your WiFi config here
+ */
+char apssid[] = "ESP";   
+char ssid[] = "HTM1";     // your network SSID (name)
+char pass[] = "kb1henna"; // your network password
+//bool WiFiAP = false;      // Do yo want the ESP as AP?
+
+int device;
+int voltage;
+int temperature;
+int humidity;
+int pressure;
+int light;
+
+/*
+ Predefined sensor type table is below:
+ volatage = 16, temperature = 26, humidity= 36, pressure= 46, light= 56, OpenClose = 66,
+ level = 76, presence = 86, motion = 96 etc.
+ */ 
+
+int sensorType1;
+int sensorType2;
+int sensorType3;
+int sensorType4;
+int sensorType5;
+
+
+//uint8_t probeData[] = {temperature, humidity, pressure, voltage, light, unit};
+
+char topic1[50];
+char topic2[50];
+char topic3[50];
+char topic4[50];
+char topic5[50];
+char topic6[50];
+
+int command1;
+int command2;
+int command3;
+int command4;
+int command5;
+int command6;
+
+//uint8_t mac[6] = {19,01,25,30,01,01};
+//uint8_t mac[6] = {19,01,250,value4,value5,01};
+uint8_t mac[6] = {command1, command2, command3, command4, command5, command6};
+
+// ==================== end of TUNEABLE PARAMETERS ====================
+
+extern "C" void preinit() {
+  wifi_set_opmode(STATIONAP_MODE);
+  wifi_set_macaddr(SOFTAP_IF, mac);
+}
+
+
+/*
+ * Custom broker class with overwritten callback functions
+ */
+class myMQTTBroker: public uMQTTBroker
+{
+public:
+    virtual bool onConnect(IPAddress addr, uint16_t client_count) {
+      Serial.println(addr.toString()+" connected");
+      return true;
+    }
+    
+    virtual bool onAuth(String username, String password) {
+      Serial.println("Username/Password: "+username+"/"+password);
+      return true;
+    }
+    
+    virtual void onData(String topic, const char *data, uint32_t length) {
+      char data_str[length+1];
+      os_memcpy(data_str, data, length);
+      data_str[length] = '\0';
+      Serial.println("received topic '" + topic + (String)data_str);
+    }
+};
+
+myMQTTBroker myBroker;
+
+WiFiEventHandler probeRequestPrintHandler;
+
+/*
+ * WiFi init stuff
+ */
+void startWiFiClient()
+{
+  Serial.println("Connecting to "+(String)ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  
+  Serial.println("WiFi connected");
+  Serial.println("IP address: " + WiFi.localIP().toString());
+}
+
+void startWiFiAP()
+{
+ 
+  WiFi.softAP(apssid, "<notused>", 7, 0, 0);   //(gateway, "<notused>", 7, 1, 0) for hidden SSID.
+  Serial.println("AP started");
+  Serial.println("IP address: " + WiFi.softAPIP().toString());
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+    //startWiFiAP();
+    startWiFiClient();
+    startWiFiAP();
+
+  probeRequestPrintHandler = WiFi.onSoftAPModeProbeRequestReceived(&onProbeRequest);
+  delay(1);
+//  wifi_set_macaddr(SOFTAP_IF, mac); 
+  
+
+
+  // Start the broker
+  Serial.println("Starting MQTT broker");
+  myBroker.init();
+
+/*
+ * Subscribe to anything
+ */
+  myBroker.subscribe("#");
+}
+
+
+void loop()
+{
+
+ 
+}
+
+
+void mqttPublish()    {
+
+/*
+ Predefined sensor type table is below:
+ volatage = 16, temperature = 26, humidity= 36, pressure= 46, light= 56, OpenClose = 66,
+ level = 76, presence = 86, motion = 96 etc.
+ */ 
+
+
+ sprintf(topic1,"%s%i%s%i%s", "Sensordata/", device, "/", device, "/");
+ sprintf(topic2,"%s%i%s%i%s", "Sensordata/", device, "/", sensorType1, "/");
+ sprintf(topic3,"%s%i%s%i%s", "Sensordata/", device, "/", sensorType2, "/");
+ sprintf(topic4,"%s%i%s%i%s", "Sensordata/", device, "/", sensorType3, "/");
+ sprintf(topic5,"%s%i%s%i%s", "Sensordata/", device, "/", sensorType4, "/");
+ sprintf(topic6,"%s%i%s%i%s", "Sensordata/", device, "/", sensorType5, "/");
+ 
+ // myBroker.publish(topic1, (String)device);
+ // myBroker.publish("SensorData/device/", (String)device);
+  myBroker.publish(topic2, (String)voltage);
+  myBroker.publish(topic3, (String)temperature);
+  myBroker.publish(topic4, (String)humidity);
+  myBroker.publish(topic5, (String)pressure);
+  myBroker.publish(topic6, (String)light);
+
+  // wait a second
+
+  delay(5000);
+}
+
+void onProbeRequest(const WiFiEventSoftAPModeProbeRequestReceived& dataReceived) {
+   
+   sendCommand(); 
+  /*
+    for(int i = 0; i < 6; i++ )
+{
+     Serial.println(dataReceived.mac[i]);
+      }
+   */
+
+  if (dataReceived.mac[1] == 16) // only accept data from device with voltage as a sensor type at byte 1.
+  {
+    sensorType1 = (dataReceived.mac[1]);
+    sensorType2 = (dataReceived.mac[2]);
+    sensorType3 = (dataReceived.mac[3]);
+    sensorType4 = (dataReceived.mac[4]);
+    sensorType5 = (dataReceived.mac[5]);
+   }
+  
+  if (dataReceived.mac[0] == 6 || dataReceived.mac[0] == 16 || dataReceived.mac[0] == 26 || dataReceived.mac[0] == 36 || dataReceived.mac[0] == 46 || dataReceived.mac[0] == 56 || dataReceived.mac[0] == 66 || dataReceived.mac[0] == 76 || dataReceived.mac[0] == 86 || dataReceived.mac[0] == 96 || dataReceived.mac[0] == 106 || dataReceived.mac[0] == 116 || dataReceived.mac[0] == 126 || dataReceived.mac[0] == 136 || dataReceived.mac[0] == 146 || dataReceived.mac[0] == 156 || dataReceived.mac[0] == 166 || dataReceived.mac[0] == 176 || dataReceived.mac[0] == 186 || dataReceived.mac[0] == 196 || dataReceived.mac[0] == 206 || dataReceived.mac[0] == 216 || dataReceived.mac[0] == 226 || dataReceived.mac[0] == 236 || dataReceived.mac[0] == 246) // only accept data from certain devices.
+  {
+    
+    Serial.print("Probe Request:- ");
+    Serial.print(" Device ID:  ");
+    Serial.print(dataReceived.mac[0], DEC);
+    device = dataReceived.mac[0];
+    Serial.print(" Voltage:  ");
+    Serial.print(dataReceived.mac[1], DEC);
+    voltage = dataReceived.mac[1];
+    voltage = voltage * 2;
+    Serial.print(" Temperature:  ");
+    Serial.print(dataReceived.mac[2], DEC);
+    temperature = dataReceived.mac[2];
+    Serial.print(" Humidity:  ");
+    Serial.print(dataReceived.mac[3], DEC);
+    humidity = dataReceived.mac[3];
+    Serial.print(" Pressure:  ");
+    Serial.print(dataReceived.mac[4], DEC);
+    pressure = dataReceived.mac[4];
+    pressure = pressure * 4;
+    Serial.print(" Light:  ");
+    Serial.println(dataReceived.mac[5], DEC);
+    light = dataReceived.mac[5];
+
+      if (voltage < 295)      // if voltage of battery gets to low, print the warning below.
+  {
+    #if MQTTBROKER
+    myBroker.publish("SensorData/warning", "Battery Low");
+    #endif
+  
+    Serial.println("**************Warning :- Battery Voltage low please change batteries********************" );
+    Serial.println();
+ 
+}  
+   if (dataReceived.mac[1] > 115 && dataReceived.mac[1] < 180)  {
+    #if MQTTBROKER
+    mqttPublish();
+    #endif
+    
+   }
+
+  } else {
+
+    //Serial.println("Waiting for Data............");
+
+  }
+   
+}
+
+void sendCommand()  {
+  command1 = 10;  //random(25);
+  command2 = 20;  //random(256);
+  command3 = 30;  //random(256);
+  command4 = 40;  //random(256);
+  command5 = random(256);
+  command6 = 01;
+  mac[0] = command1;
+  mac[1] = command2;
+  mac[2] = command3;
+  mac[3] = command4;
+  mac[4] = command5;
+  mac[5] = command6;
+
+  wifi_set_macaddr(SOFTAP_IF, mac);
+
+}
---
+++ examples/ProbeSender.ino
@@ -0,0 +1,205 @@
+
+ADC_MODE(ADC_VCC); //vcc read-mode
+//#define SLEEP_SECS 15 * 60 // 15 minutes
+
+#define DUPLEX    true   // true if two way communication required with controller (around 150 milliseconds as opposed to 60 milliseonds if false).
+
+uint8 gateway[32] = "ESP";   // This name has to be same as main controller's ssid.
+int apChannel = 7;           // This channel has to be same as main controller's channel & AP where main controller is connected for internet.
+int sleepTime = 100;         // Sleep time in seconds 
+// use any of following for devie ID ending with 6.
+// 6,16,26,36,46,56,66,76,86,96,106,116,126,136,146,156,166,176,186,196,206,216,226,236,246 etc.
+
+int device = 36;    // device ID must end with 2,6,A or E. See https://serverfault.com/questions/40712/what-range-of-mac-addresses-can-i-safely-use-for-my-virtual-machines.
+
+// Values to be sent to Gateway
+int voltage;
+int temperature;
+int humidity;
+int pressure;
+int light;
+//int openClose = 0;    // 0 or 1
+//int level;            // 0 to 255
+//int presence = 0;     // 0 or 1
+//int motion = 0;       // 0 to 6 (0=no motion, 1=up, 2=down, 3=right, 4=left, 5=on, 6=off).
+
+uint8_t sensorData[6] = {device, voltage, temperature, humidity, pressure, light};
+uint8_t sensorType[6] = {device, 6, 16, 26, 36, 46}; // Change last 4 bytes according to sensot type used.
+// Predefined sensor type table is below:
+// volatage = 6, temperature = 16, humidity= 26, pressure= 36, light= 46, OpenClose = 56,
+// level = 66, presence = 76, motion = 86 etc.
+
+//uint8_t securityCode[6] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33}; // Security code must be same at controller to compare.
+
+int receivedCommand;  // digitalwrite, analogwrite, pin setup etc.
+int pin;              // gpio pin number 1 to 16
+int pinValue;         // 0 or 1 in case of digitalwrte or 0 to 255 in case of analogwrite.
+int setSleeptime;  // 0 to 255 command to set sleep time variable in minutes for this device.
+int setDefines;    // Set Defines (whether to run ertain code to run WiFi & OTA options etc.).
+
+int ledPin = 12;
+//int lowVolt = 0;
+//int highVolt = 330;   // highest voltage of battery used multiply by 100.
+int warnVolt = 130;    // Start warning when battery level goes below 2.60 volts (260/2 = 130).
+unsigned long lastMillis;
+unsigned long passedMillis;
+
+
+#include <ESP8266WiFi.h>
+
+
+//============Do not need user configuration from here on============================
+
+void setup() {
+  // WiFi.disconnect();
+  WiFi.scanDelete();
+  Serial.begin(115200);
+ /*
+  wifi_set_macaddr(STATION_IF, securityCode);
+  probeRequest();
+  Serial.print("Security Code sent to controller: ");
+  Serial.println(WiFi.macAddress());
+  */
+  wifi_set_macaddr(STATION_IF, sensorType);
+  probeRequest();
+  Serial.print("sensorType values sent to controller: ");
+  Serial.println(WiFi.macAddress());
+  
+  sensorValues();
+  probeRequest();
+  //Serial.print("Sensor values sent to controller: ");
+  //Serial.println(WiFi.macAddress());
+
+#if DUPLEX
+  delay(60);  // Minimum 60 milliseonds delay required to receive message from controller reliably.
+
+  if (WiFi.BSSIDstr(0)[16] == '1')  {   //match last digit of gateway's mac id with this devices's ID here.
+
+    Serial.println();
+    Serial.print("This Device MAC ID is: ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("This Device Name is: ");
+    Serial.println(WiFi.hostname());
+    Serial.print("Gateway Name is: ");
+    Serial.println(WiFi.SSID(0));
+    Serial.print("Gateway Channel is: ");
+    Serial.println(WiFi.channel(0));
+    Serial.print("Message sent to Controller is: ");
+    Serial.println(WiFi.macAddress());
+    Serial.print("Message received from Controller is: ");
+    Serial.println(&WiFi.BSSIDstr(0)[0]);
+
+    if (WiFi.BSSIDstr(0)[0] == '0')  {
+      Serial.print("GPIO,");
+      Serial.print(ledPin);
+      Serial.print(",");
+      Serial.println(WiFi.BSSIDstr(0)[15]);
+    }
+
+    if (WiFi.BSSIDstr(0)[16] == '1')  {
+      Serial.println("I will wake up next time with WiFi, OTA enabled and deepsleep disabled");
+      Serial.println("Define some features here to activate wifi  & disable deepsleep when unit wakes up next time then restart the ESP.");
+      // ESP.restart();
+    }
+
+
+  } else {
+    Serial.println("Message from controller did not arrive, let me try again to get message data........................................");
+    ESP.restart();
+
+  }
+
+  
+#endif
+
+  delay(1);
+
+  WiFi.hostname("Livingroom");
+  Serial.println();
+}
+
+// Setup ends here
+
+//========================Main Loop================================
+
+void loop() {
+
+  //  probeRequest();
+  //  yield();
+
+  Serial.print("Total time I spent before going to sleep: ");
+  Serial.println(millis());
+  Serial.print("I will wakeup in: ");
+  Serial.print(sleepTime);
+  Serial.println(" Seconds");
+
+  delay(5000);         //disable delay when deep sleep activated.
+  Serial.println("Going to Deep Sleep..........................");
+  ESP.restart();
+  //ESP.deepSleep(0); //If last digit of MAC ID matches to device ID go to deep sleep else loop through again.
+}
+
+//=========================Main Loop ends==========================
+
+
+void sensorValues()     {
+
+
+  // voltage = random(100);
+  float voltage = ESP.getVcc() / (float)1023 * 50; // * (float)1.07;
+  //voltage = map(voltage, lowVolt, highVolt, 0, 100);
+
+  Serial.print("Voltage: "); Serial.print(voltage); Serial.print("  Minimum Voltage Required: "); Serial.println(warnVolt);
+  if (voltage < warnVolt)      // if voltage of battery gets to low, print the warning below.
+  {
+    Serial.println("Warning :- Battery Voltage low please change batteries" );
+    Serial.println();
+  }
+
+  temperature = random(90);
+  humidity = random(100);
+  pressure = random(1024);
+  pressure = pressure / 4;
+  light = random(100);
+  //sensorData[0] = device;
+  sensorData[1] = voltage;
+  sensorData[2] = temperature;
+  sensorData[3] = humidity;
+  sensorData[4] = pressure;
+  sensorData[5] = light;
+  wifi_set_macaddr(STATION_IF, sensorData);
+
+}
+
+
+void gotoSleep() {      //need connection between GPIO16 and reset pin of ESP8266
+  // add some randomness to avoid collisions with multiple devices
+  int sleepSecs = sleepTime;// + ((uint8_t)RANDOM_REG32/2);
+  Serial.printf("Up for %i ms, going to sleep for %i secs...\n", millis(), sleepSecs);
+  ESP.deepSleep(sleepSecs * 1000000, RF_NO_CAL);
+  ////ESP.deepSleep( SLEEPTIME, WAKE_RF_DISABLED );
+}
+
+
+//=========================Probe request function starts===========
+
+
+void probeRequest()  {
+  /*
+    Serial.println("Starting Probe sender");
+    Serial.println("Sending sensor data over Probe request protocol to Master Node");
+    Serial.println();
+  */
+  //int8_t scanNetworks(bool async = true, bool show_hidden = false, uint8 channel = 0, uint8* ssid = NULL);
+ int n = WiFi.scanNetworks(true, false, apChannel, (uint8*) gateway);
+
+  yield();
+
+  Serial.println();
+  WiFi.scanDelete();
+  passedMillis = millis() - lastMillis;
+  Serial.print("Time spent on Probe Request: ");
+  Serial.println(passedMillis);
+  lastMillis = millis();
+  Serial.println();
+}
